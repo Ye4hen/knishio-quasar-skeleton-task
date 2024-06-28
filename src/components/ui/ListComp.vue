2cf0978fcee7774144de35c42bd38707a32baae6
Yevhen Bosak <125126424+Ye4hen@users.noreply.github.com>
2024-06-28 14:43:07 +0200

    Update ListComp.vue

--- src/components/ui/ListComp.vue
+++ src/components/ui/ListComp.vue
@@ -1,23 +1,34 @@
 // Reusable kit for the lists
 <template>
 	<listTag>
-		<li v-for="(item, index) in listArray" :key="isIndexKey ? index : item" class="q-mb-md text-h5">
-			<span>{{item}}</span>
-            <q-btn
-              v-if="isDeleteBtn"
-              type="button"
-              :label="deleteBtn"
-              size="0.9375rem"
-              padding="2.5px 10px"
-              class="q-ml-sm"
-              @click="deleteTask(index)"
-           />
-		</li>
+    <li v-for="(item, index) in listArray" :key="isIndexKey ? index : item" class="q-mb-md">
+      <q-card>
+        <q-card-section :class="!multipleKeys ? 'flex justify-between' : 'column'">
+          <template v-if="multipleKeys">
+            <div v-for="(val, key, subIndex) in item" :key="isIndexKey ? subIndex : item">
+              <slot name="itemSlot" :key="key" :value="val"></slot>
+            </div>
+          </template>
+          <template v-else>
+            <div class="text-h6">{{index + 1}}. {{item}}</div>
+          </template>
+          <q-btn
+            v-if="isDeleteBtn"
+            type="button"
+            :label="deleteBtn"
+            size="0.9375rem"
+            padding="2.5px 10px"
+            class="q-ml-sm"
+            @click="deleteTask(index)"
+          />
+        </q-card-section>
+      </q-card>
+    </li>
 	</listTag>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watchEffect } from 'vue'
 // import hasDltStore from 'src/mixins/hasDltStore'
 import { useQuasar } from 'quasar'
 // const { dltStore } = hasDltStore()
@@ -36,6 +47,23 @@ const props = defineProps({
   deleteBtn: String,
   deleteMethod: Function
 })
+
+const emit = defineEmits(['keyValueEmitted'])
+
+function emitKeyValue (key, value) {
+  emit('keyValueEmitted', { key, value })
+}
+
+function processListArray () {
+  if (props.multipleKeys) {
+    props.listArray.forEach(item => {
+      Object.entries(item).forEach(([key, value]) => {
+        emitKeyValue(key, value)
+      })
+    })
+  }
+}
+watchEffect(props.listArray, processListArray())
 
 const listTag = computed(() => {
   return props.isListOrdered ? 'ol' : 'ul'
